4524c0cb7a3ef6b68d541f151dc4b0b656980728
ly <goh.lingyong@stee.stengg.com>
2019-01-29 15:51:12 +0800

    Update quickenquire

--- TrueMedical Patient/www/scripts/quickenquire.js
+++ TrueMedical Patient/www/scripts/quickenquire.js
@@ -11,21 +11,49 @@ app.controller('patientCtrl', function ($scope, $http) {
         };
         
         var url = serverURL() + "/enquire.php";
+
+
+        if (checkCorrectness()) {
+            $.ajax({
+                url: url,
+                type: 'GET',
+                data: data,
+                dataType: 'json',
+                contentType: "application/json; charset=utf-8",
+                success: function (arr) {
+                    alert("Your query is sent");
+                    window.location.href = "homepage.html";
+                    return;
+                },
+                error: function (arr) {
+                    alert("Your query is sent");
+                    window.location.href = "homepage.html";
+                    return;
+                }
+            });
+        }
         
-        $.ajax({
-            url: url,
-            type: 'GET',
-            data: data,
-            dataType: 'json',
-            contentType: "application/json; charset=utf-8",
-            success: function (arr) {
-                return;
-            },
-            error: function () {
-                return;
+
+        var checkCorrectness = function () {
+            if ($scope.name == "") {
+                alert("Name is empty");
+                return false;
             }
-        });
-        window.location.href = "homepage.html";
+            if ($scope.email == "") {
+                alert("Email is empty");
+                return false;
+            }
+            if ($scope.phoneNo == "") {
+                alert("Phone Number is empty");
+                return false;
+            }
+            if ($scope.enquiry == "") {
+                alert("Enquiry is empty");
+                return false;
+            }
+            return true;
+        }
+        
     }
     
 });
