645c191713a5ccd276cae86977cd4bc905f60cb0
ly <goh.lingyong@stee.stengg.com>
2019-01-29 16:54:36 +0800

    AD

--- TrueMedical Patient/www/scripts/quickenquire.js
+++ TrueMedical Patient/www/scripts/quickenquire.js
@@ -2,7 +2,13 @@
 
 var app = angular.module('myApp', []);
 app.controller('patientCtrl', function ($scope, $http) {
-    $scope.btnEnquire = function () {
+    printnavbar();
+
+    $scope.name = "";
+    $scope.email = "";
+    $scope.phoneNo = "";
+    $scope.enquiry = "";
+    $scope.btnEnquire3 = function () {
         var data = {
             "name": $scope.name,
             "email": $scope.email,
@@ -12,6 +18,25 @@ app.controller('patientCtrl', function ($scope, $http) {
         
         var url = serverURL() + "/enquire.php";
 
+        var checkCorrectness = function () {
+            if ($scope.name == "" || $scope.name == undefined) {
+                alert("Name is empty");
+                return false;
+            }
+            if ($scope.email == "" || $scope.email == undefined) {
+                alert("Email is empty");
+                return false;
+            }
+            if ($scope.phoneNo == "" || $scope.phoneNo == undefined) {
+                alert("Phone Number is empty");
+                return false;
+            }
+            if ($scope.enquiry == "" || $scope.enquiry == undefined) {
+                alert("Enquiry is empty");
+                return false;
+            }
+            return true;
+        }
 
         if (checkCorrectness()) {
             $.ajax({
@@ -22,35 +47,15 @@ app.controller('patientCtrl', function ($scope, $http) {
                 contentType: "application/json; charset=utf-8",
                 success: function (arr) {
                     alert("Your query is sent");
-                    return;
                 },
                 error: function (arr) {
                     alert("Your query is sent");
-                    return;
                 }
             });
         }
         
 
-        var checkCorrectness = function () {
-            if ($scope.name == "") {
-                alert("Name is empty");
-                return false;
-            }
-            if ($scope.email == "") {
-                alert("Email is empty");
-                return false;
-            }
-            if ($scope.phoneNo == "") {
-                alert("Phone Number is empty");
-                return false;
-            }
-            if ($scope.enquiry == "") {
-                alert("Enquiry is empty");
-                return false;
-            }
-            return true;
-        }
+       
         
     }
     
